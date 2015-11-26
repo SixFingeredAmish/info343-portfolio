50324a7fadf532a8501c3cd1209b88337263cc3f
Nirawit Jittipairoj <sixfingeredamish@gmail.com>
2015-11-26 12:12:26 -0800

    thunderstruck

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Create app
+var myApp = angular.module('myApp', ['ui.router'])
+
+// Configure app
+.config(function($stateProvider) {
+    $stateProvider
+    .state('home', {
+        url:'/home',
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController',
+    })
+    // Configure states for "content" and "about"
+    .state('content', {
+        url:'/portfolio',
+        templateUrl: 'templates/content.html',
+        controller: 'ContentController',
+    })
+
+    .state('about', {
+        url:'/about',
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController',
+    })
+});
+
+// Landing page controller: define $scope.number as a number
+myApp.controller("HomeController", function($scope, $http) {
+  $http.get('/data/data.json').success(function(data) {
+		$scope.data = data;
+	});
+})
+
+// About page controller: define $scope.about as a string
+.controller('AboutController', function($scope){
+
+})
+
+// Content controller: define $scope.url as an image
+.controller("ContentController", function($scope) {
+
+})
